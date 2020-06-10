1e8c7d95f8082892c947c3c5e5373b270a2b8c6a
Yauheni Siarhei <ysiarhei@iba.by>
2020-06-10 16:40:27 +0300

    fix getting token

--- get_token_apiurl.sh
+++ get_token_apiurl.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 
-echo "token"
-kubectl -n kube-system describe secrets zabbix-user-token-t9rrw | grep "token:" | awk '{print $2}'
-
-echo -e "\nAPI server url"
+echo -e "Token:\n"
+kubectl -n kube-system describe secrets `kubectl -n kube-system get secrets | grep "zabbix" | awk '{print $1}'` | grep "token:" | awk '{print $2}'
+echo -e "\nAPI server url:\n"
 kubectl cluster-info | grep -i "Kubernetes master" | grep -o '[a-z]\{4,5\}:\/\/[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}:[0-9]\{1,5\}'
